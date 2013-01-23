e3ac0b435494047e8515e58bb2aec5796fcdc173
Chris Hannon <himynameschris@gmail.com>
2013-01-22 23:47:54 -0500

    task: add basic dll project

---
+++ socket.io-poco/SIOClient.cpp
@@ -0,0 +1,12 @@
+#include "StdAfx.h"
+#include "SIOClient.h"
+
+
+SIOClient::SIOClient(void)
+{
+}
+
+
+SIOClient::~SIOClient(void)
+{
+}
---
+++ socket.io-poco/socket.io-poco.cpp
@@ -0,0 +1,5 @@
+// socket.io-poco.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+
