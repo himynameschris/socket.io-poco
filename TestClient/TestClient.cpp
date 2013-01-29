779ba941e4e3df07ed2f84f6284075da2c63af09
Chris Hannon <himynameschris@gmail.com>
2013-01-28 19:31:50 -0500

    task: update program loop in testclient

--- TestClient/TestClient.cpp
+++ TestClient/TestClient.cpp
@@ -1,16 +1,22 @@
 // TestClient.cpp : Defines the entry point for the console application.
 //
+#include "StdAfx.h"
 
-#include "stdafx.h"
 #include "SIOClient.h"
 
+#include <iostream>
 
-
-int _tmain(int argc, _TCHAR* argv[])
+int _tmain(int argc, char* argv[])
 {
 	SIOClient *sio = new SIOClient(3000, "localhost");
 
+	do 
+	{
+		sio->receive();
 
+	} while (true);
+
+	sio->pauser();
 
 	return 0;
 }
