0af42b8cb271d6075d3aa4f6f5f28afea3a7e634
Chris Hannon <himynameschris@gmail.com>
2013-03-25 21:33:30 -0400

    task: implement use of thread in test client for socket monitoring method

--- TestClient/TestClient.cpp
+++ TestClient/TestClient.cpp
@@ -3,14 +3,30 @@
 #include "StdAfx.h"
 
 #include "SIOClient.h"
+#include "Poco/RunnableAdapter.h"
+#include "Poco/Thread.h"
 
 #include <iostream>
+
+using Poco::Thread;
+
+Poco::Thread;
 
 int _tmain(int argc, char* argv[])
 {
 	SIOClient *sio = new SIOClient(3000, "localhost");
 
-	sio->pauser();
+	sio->connect();
+
+	Thread thread;
+
+	Poco::RunnableAdapter<SIOClient> runnable(*sio, &SIOClient::monitor);
+
+	thread.start(runnable);
+
+	do{
+
+	}while(true);
 
 	return 0;
 }
