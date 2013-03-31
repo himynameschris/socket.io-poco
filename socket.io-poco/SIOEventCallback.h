14d639d0150a701cfdcd6a4f9a9e8ddb1562457e
Chris Hannon <himynameschris@gmail.com>
2013-03-31 12:07:51 -0400

    task: create event callback class to receive event notifications

---
+++ socket.io-poco/SIOEventCallback.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "SIOEventRegistry.h"
+#include "SIOClient.h"
+
+/**
+ * This class provides an interface to call event functions
+ */
+class SIOEventCallbackBase
+{
+public:
+    virtual ~SIOEventCallbackBase()
+    {}
+
+    virtual void fire( SIOClient *client, std::string *data ) = 0;
+};
+
+/**
+ * This template class adapts to the destination object and provides
+ * a method to call the registered function
+ */
+template <class T> class SIOEventCallback : public SIOEventCallbackBase
+{
+public:
+
+    /**
+     * Register a callback function to be called from collide()
+     * @param ftp pointer to the method to call
+     */
+    SIOEventCallback(void(T::*fpt)( std::string *data ))
+    {
+        mFpt=fpt;
+    };
+
+    /**
+     * Calls the registered collision function on receiver,
+     * using otherObject and c as parameter
+     * @param receiver object to call the callback on
+     * @param otherObject parameter to pass to the callback function
+     * @param c collision parameter to pass to the callback object
+     */
+    virtual void fire(SIOClient *client, std::string *data)
+    {
+        T* r = dynamic_cast<T*>(client);
+        (*r.*mFpt)(data);
+    };
+
+private:
+   void(T::*mFpt)(std::string *data);
+};
+
+/**
+ * Macro to register the A:onB()
+ * @param A class to call the method on
+ * @param B class which A collides with
+ */
+#define ON_EVENT(A,B) static bool sioOn_##A##_##B##_registered = SIOEventRegistry::sharedInstance()->registerEvent(#A"::on"#B,new SIOEventCallback<A>(&A::on##B));
