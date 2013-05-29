7453a0801d76c23f69d98c476b6c13e9e6df8d2f
Chris Hannon <himynameschris@gmail.com>
2013-05-28 23:45:02 -0400

    task: add documentation and comments to the test client app

--- TestClient/main.cpp
+++ TestClient/main.cpp
@@ -12,23 +12,33 @@ using Poco::Thread;
 
 int main(int argc, char* argv[])
 {
+	//create a c++ Poco logger to use and set its channel to the windows console
+	//this is the same logger instance that the library will hook into
 	Logger *logger = &(Logger::get("SIOClientLog"));
 	logger->setChannel(new WindowsConsoleChannel());
 
+	//Establish the socket.io connection
+	//JS: var socket = io.connect("localhost:3000")
 	SIOClient *sio = SIOClient::connect("http://localhost:3000");
 
+	//Create a target and register object its method onUpdate for the Update event
+	//JS: socket.on("Update", function(data) {...});
 	TestTarget *target = new TestTarget();
-
 	sio->on("Update", target, callback(&TestTarget::onUpdate));
-		
-	logger->information("sio setup complete, sending message");
+	
+	//setup is now complete, messages and events can be send and received
+	logger->information("sio setup complete\n");
 
+	//test the message sending
+	logger->information("sending message\n");
 	sio->send("Hello Socket.IO");
 
-	logger->information("emitting test event");
-
+	//test the event emitter
+	logger->information("emitting test event\n");
 	sio->emit("testevent", "[{\"name\":\"myname\",\"type\":\"mytype\"}]");
 
+	//enter a do loop to keep the program and connection alive
+	//ctrl+c or close window to end the program and close the connection
 	logger->information("entering loop");
 	
 	do{
